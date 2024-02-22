606ba14fd4274ff9e08eee83fb187d0c496502fe
Ragnar Öövel <oovel@rushstreetinteractive.com>
2024-02-22 15:02:06 +0200

    add method to find bookmarks

--- src/main/java/com/spring/springbootrestbestpractices/BookmarkRepository.java
+++ src/main/java/com/spring/springbootrestbestpractices/BookmarkRepository.java
@@ -1,6 +1,16 @@
 package com.spring.springbootrestbestpractices;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    @Query("""
+               SELECT
+                new com.spring.springbootrestbestpractices.BookmarkDTO(b.id, b.title, b.url, b.createdAt)
+               FROM Bookmark b
+            """)
+    Page<BookmarkDTO> findBookmarks(Pageable pageable);
 }
