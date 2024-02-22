aba69f0dbf72a35cfdfa1285feb942a85b6ee7ff
Ragnar Öövel <oovel@rushstreetinteractive.com>
2024-02-22 14:18:51 +0200

    add Bookmark entity

---
+++ src/main/java/com/spring/springbootrestbestpractices/Bookmark.java
@@ -0,0 +1,72 @@
+package com.spring.springbootrestbestpractices;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "bookmark")
+class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private String createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private String updatedAt;
+
+    public Bookmark() {
+    }
+
+    public Bookmark(Long id, String title, String url, String createdAt, String updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
