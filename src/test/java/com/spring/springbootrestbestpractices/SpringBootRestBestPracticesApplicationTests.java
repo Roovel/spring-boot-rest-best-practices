34a12176734462bbb36af9365654219ca5192dc5
Ragnar Öövel <oovel@rushstreetinteractive.com>
2024-02-22 10:59:25 +0200

    Initial commit

---
+++ src/test/java/com/spring/springbootrestbestpractices/SpringBootRestBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootrestbestpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestBestPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/spring/springbootrestbestpractices/TestSpringBootRestBestPracticesApplication.java
@@ -0,0 +1,26 @@
+package com.spring.springbootrestbestpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestSpringBootRestBestPracticesApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication
+                .from(SpringBootRestBestPracticesApplication::main)
+                .with(TestSpringBootRestBestPracticesApplication.class)
+                .run(args);
+    }
+
+}
